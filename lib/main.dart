26bf6eb970f101580b0637552c78262dc8c268f4
TavGit <gustavbaim18@gmail.com>
2025-03-27 12:49:52 +0700

    update aplikasi sederhana

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:aplikasi_sederhana/Authentication/auth_gate.dart';
 // import 'package:aplikasi_sederhana/Daftar/daftar_pages.dart';
 // import 'package:aplikasi_sederhana/Login/login_pages.dart';
 // import 'package:aplikasi_sederhana/Home/Home_Pages.dart';
-import './BottomNavBar/bottomnavbar.dart';
+// import './BottomNavBar/bottomnavbar.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttcnVxcmhwcGxzbW1hY2RqdWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNTA2NDIsImV4cCI6MjA1ODYyNjY0Mn0.7XOVtt_NJKRG2bQCnOomlMtbIGmYiwaJfo4U-mZhmj0",
+    url: "https://kmruqrhpplsmmacdjukj.supabase.co",
+  );
   runApp(const MyApp());
 }
 
@@ -16,9 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyBottomNavBar(),
+      home: AuthGate(),
     );
   }
 }
-
-
